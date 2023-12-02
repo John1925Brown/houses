ec0a4ddb52ee3455732e0497796a66efb51fca0f
Dzmitry Yarasheuski <materik08082020@gmail.com>
2023-12-02 04:31:27 +0300

    config webp, config syntax after webp config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,54 @@
-const path = require("path");
-const miniCss = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: "./src/index.js",
+const devServer = (isDev) => !isDev ? {} : {
+    devServer: {
+        open: true,
+        hot: true,
+        port: 8080,
+    }
+};
+
+module.exports = ({develop}) => ({
+  mode: develop ? 'development' : 'production',
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(s*)css$/,
-        use: [miniCss.loader, "css-loader", "sass-loader"],
-      },
-    ],
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
   },
   plugins: [
-    new miniCss({
-      filename: "style.css",
-    }),
+      new HtmlWebpackPlugin({
+          template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin({
+          filename: './styles/main.css'
+      })
   ],
-};
+  module: {
+      rules: [
+          {
+              test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
+              type: 'asset/inline'
+          },
+          {
+              test: /\.html$/i,
+              loader: 'html-loader'
+          },
+          {
+              test: /\.css$/i,
+              use: [
+                MiniCssExtractPlugin.loader, 'css-loader'
+              ]
+          },
+          {
+              test: /\.scss$/i,
+              use: [
+                MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+              ]
+          }
+      ]
+  },
+  ...devServer(develop),
+});
